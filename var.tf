52424b2faa0b5a4e6be4db23b187a1b59cad6dac
root <root@ip-172-31-46-181.ap-south-1.compute.internal>
2020-07-23 07:32:45 +0000

    rg tf

---
+++ var.tf
@@ -0,0 +1,15 @@
+variable "subscription_id" {
+description = "subscription id of the account"
+}
+
+variable "client_id" {
+description = "client id"
+}
+
+variable "client_secret" {
+description = "client secret"
+}
+
+variable "tenant_id" {
+ description = "tenant id"
+}
